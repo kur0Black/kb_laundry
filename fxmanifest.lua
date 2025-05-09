04cc68a704564282ec96455cb3959b2f632cb72b
Bruno Mellara <44076984+kur0Black@users.noreply.github.com>
2025-05-09 15:59:51 -0300

    Preparacao de arquitetura de script

---
+++ fxmanifest.lua
@@ -0,0 +1,12 @@
+fx_version 'ceruleam'
+game {'gta5'}
+
+author 'kur0_black'
+description 'A laundry script in BETA.'
+version '0.1'
+
+client_script {'client/*.lua'}
+
+server_scripts {'server/*.lua'}
+
+shared_scripts {'shared/*.lua'}
